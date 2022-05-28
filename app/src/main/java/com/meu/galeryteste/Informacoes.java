ae2485e2286dedc8326c7c12bd7aac31984e8959
Anderson <andersantfer@gmail.com>
2022-05-28 18:55:18 -0300

    atualizando interface

--- app/src/main/java/com/meu/galeryteste/Informacoes.java
+++ app/src/main/java/com/meu/galeryteste/Informacoes.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class Informacoes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacoes);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
     }//onCreate
 
